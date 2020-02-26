e10995319b43508009a44e61ba4aa1c0bac27c23
Steven Houng <syhoung1@gmail.com>
2020-02-25 18:12:47 -0800

    updates to model fields

--- noteme/main/models.py
+++ noteme/main/models.py
@@ -1,24 +1,30 @@
 from django.db import models
 
 # Create your models here.
+# checked
 class Topics(models.Model):
     name = models.CharField(max_length=80)
-    description = models.CharField(max_length=150)
+    description = models.CharField(max_length=150, blank=True)
     parent_topic = models.ForeignKey(
         'self',
         on_delete=models.CASCADE,
+        null=True
     )
 
 class Notes(models.Model):
     title = models.CharField(max_length=80)
-    content = models.TextField(null=True)
+    content = models.TextField(blank=True, default=None)
     topic = models.ForeignKey(
         Topics,
-        on_delete = models.CASCADE
+        on_delete = models.CASCADE,
     )
     parent_note = models.ForeignKey(
         'self',
         on_delete = models.CASCADE,
+        null=True
+    )
+    related_note = models.ManyToManyField(
+        'self',
     )
 
 class Vocab(models.Model):
