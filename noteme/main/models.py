97d0a74687f36edff5a76ee740bbd13ceb58598d
Steven Houng <syhoung1@gmail.com>
2020-02-26 17:08:57 -0800

    Add user class

--- noteme/main/models.py
+++ noteme/main/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
-# checked
+
+class Users(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True)
+    phone_number = models.IntegerField(blank=True)
+
 class Topics(models.Model):
     name = models.CharField(max_length=80)
     description = models.CharField(max_length=150, blank=True)
@@ -9,11 +14,11 @@ class Topics(models.Model):
         'self',
         on_delete=models.CASCADE,
         null=True
-    )
+    )    
 
 class Notes(models.Model):
     title = models.CharField(max_length=80)
-    content = models.TextField(blank=True, default=None)
+    content = models.TextField(blank=True)
     topic = models.ForeignKey(
         Topics,
         on_delete = models.CASCADE,
@@ -29,8 +34,8 @@ class Notes(models.Model):
 
 class Vocab(models.Model):
     word = models.CharField(max_length=50)
-    part_of_speech = models.CharField(max_length=20)
-    definition = models.CharField(max_length=100)
+    part_of_speech = models.CharField(max_length=20, blank=True)
+    definition = models.CharField(max_length=100, blank=True)
     topic = models.ForeignKey(
         Topics,
         on_delete = models.CASCADE
